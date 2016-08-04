6753a2147ad42ad84e101a6bd800cddefb04814a
jmoraes <jmoraes@ciandt.com>
2016-08-04 16:37:45 -0300

    update class HelloWorldFacade setup config redis

--- src/main/java/br/com/cit/resteasy/facade/HelloWorldFacade.java
+++ src/main/java/br/com/cit/resteasy/facade/HelloWorldFacade.java
@@ -7,17 +7,40 @@ import javax.ws.rs.Produces;
 import org.springframework.stereotype.Component;
 
 import br.com.cit.resteasy.bean.HelloBean;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Component
 @Path("/hello")
 public class HelloWorldFacade {
+	private final JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+
+	private Integer countRedis() {
+		Jedis jedis = null;
+		try {
+			jedis = pool.getResource();
+			final Object value = jedis.get("count");
+			Integer count = 0;
+			if (value != null) {
+				count = Integer.parseInt(value.toString());
+			}
+			count++;
+			jedis.set("count", count.toString());
+			return count;
+		} finally {
+			if (jedis != null) {
+				jedis.close();
+			}
+		}
+	}
 
 	@GET
 	@Produces("application/json")
 	@Path("/world")
 	public HelloBean teste() {
 		final HelloBean bean = new HelloBean();
-		bean.setText("Hello World!");
+		bean.setText("Hello World! Count: " + countRedis());
 		return bean;
 	}
 }
